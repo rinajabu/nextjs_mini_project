3b3762329da29457ab561ee5576c8b15b5e1333a
Rina Joy Abu <rinajoyyy@Rinas-MacBook-Pro.local>
2021-09-27 11:51:19 -0400

    about me section done

--- pages/index.js
+++ pages/index.js
@@ -14,25 +14,34 @@ export default function Home() {
       <main className={styles.main}>
 
         <nav className={styles.nav}>
-
           <h1>Rina Joy Abu</h1>
-
           <div>
             <a href="">About</a>
             <a href="">Skills</a>
             <a href="">Projects</a>
             <a href="">Contact</a>
           </div>
-
         </nav>
 
         <section className={styles.section__about}>
-
           <div>
             <h2>Rina Joy Abu</h2>
             <h4>Software Engineer</h4>
           </div>
-        
+        </section>
+
+        <section className={styles.section__aboutMe}>
+          <div>
+            <h4>About Me</h4>
+            <p>Driven by my compassion for my family and close friends, I see challenges through a growth mindset lens and believe my abilities can always be developed through hard work and dedication. With the problem solving skills I’ve learned in my past experience in the medical coding world, I’m motivated to use my new fluencies in various programming technologies to join a software engineering team where we can help establish a more efficient and accessible world.</p>
+          </div>
+          <Image src='/headshot.jpg' alt='me' height='950' width='750' />
+        </section>
+
+        <section className={styles.section__contact}>
+          <h4>Contact Me</h4>
+          <p>Email me at: rinajabu@gmail.com</p>
+          <p>Or send me a message on LinkedIn!</p>
         </section>
         
       </main>
