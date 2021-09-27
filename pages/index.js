d30b921eaf6fd6d07e2235205466b4fe6df2b42f
Rina Joy Abu <rinajoyyy@Rinas-MacBook-Pro.local>
2021-09-27 10:26:13 -0400

    nav and first section done

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
@@ -13,18 +13,27 @@ export default function Home() {
 
       <main className={styles.main}>
 
-        <nav>
-        
+        <nav className={styles.nav}>
+
           <h1>Rina Joy Abu</h1>
 
           <div>
-            <a href="">About Me</a>
+            <a href="">About</a>
             <a href="">Skills</a>
             <a href="">Projects</a>
             <a href="">Contact</a>
           </div>
 
         </nav>
+
+        <section className={styles.section__about}>
+
+          <div>
+            <h2>Rina Joy Abu</h2>
+            <h4>Software Engineer</h4>
+          </div>
+        
+        </section>
         
       </main>
 
